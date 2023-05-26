75a1420047dcb818a341e730cdba9240e2fcbff3
jenishbekdev <nurikovn87@gmail.com>
2023-05-27 01:28:13 +0600

    add adaptation

--- app.js
+++ app.js
@@ -3,20 +3,22 @@ const input = document.querySelector("input");
 const hero = document.querySelector(".hero");
 const select = document.querySelector("select");
 let all = null
+
 function getApi(API) {
-  axios(`https://restcountries.com/v3.1/${API}`)
-  .then((res) => {
-    console.log(res.data);
-    all = res.data
-    view(res.data);
-  });
+    axios(`https://restcountries.com/v3.1/${API}`)
+        .then((res) => {
+            console.log(res.data);
+            all = res.data
+            view(res.data);
+        });
 }
+
 getApi("all");
 
 function view(data) {
     hero.innerHTML = ''
-  data.map((el) => {
-    hero.innerHTML += `
+    data.slice(0, 10).map((el) => {
+        hero.innerHTML += `
         <div class="box">
             <img src="${el.flags.svg}" width='200px'alt="">
             <h1>${el.name.common}</h1>
@@ -26,8 +28,9 @@ function view(data) {
             
         </div>
         `;
-  });
+    });
 }
+
 btn.addEventListener('click', () => {
     getApi(`name/${input.value}`)
 })
@@ -60,7 +63,7 @@ select.addEventListener('change', (e) => {
     let target = e.target.value
     if (target === 'A-Z') {
         const res = all.sort((a, b) => {
-            return a.name.common > b.name.common ? 1 : -1;        
+            return a.name.common > b.name.common ? 1 : -1;
         })
         view(res)
     }
@@ -84,4 +87,27 @@ select.addEventListener('change', (e) => {
         })
         view(res)
     }
+})
+
+// burger
+
+const burgerMenu = document.querySelector('.header__burger')
+const headerNav = document.querySelector('.menu')
+const burgerItem = document.querySelectorAll('.burger__item')
+const body = document.querySelector('body')
+burgerMenu.addEventListener('click', (e) => {
+    body.addEventListener('click', () => {
+        if (e.target.classList.toggle('header__burger')) {
+            headerNav.style.display = 'flex'
+            headerNav.style.gap = "20px"
+            burgerItem[0].style.transform = 'rotate(-50deg) translateX(-3px)'
+            burgerItem[2].style.transform = 'rotate(50deg)translateY(1px)'
+            burgerItem[1].style.display = 'none'
+        } else {
+            headerNav.style.display = 'none'
+            burgerItem[0].style.transform = 'rotate(0)'
+            burgerItem[2].style.transform = 'rotate(0)'
+            burgerItem[1].style.display = 'block'
+        }
+    })
 })
